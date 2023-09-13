49a48dea7290e71fcafd73b5e875fe54edf7b39b
William Franklin <emailwilliamfranklin@gmail.com>
2023-09-12 22:28:42 -0700

    Updated with additional refinement to and first pass for pulling data from FBRef table and updated team objects

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Workflow Goal:
 - Python (BS4) to get data from FBRef sites for each team
 - Python to interpret and organize data pulled for each team.
 - HTML/CSS/JS to create web app to view stats by team, or compare league and player stats.
+
+Current Workflow:
+
+- Requests main Premier League page on FBREf
+- Function (pullTable) to select each table on the page
+- Function (rowData) to pull stats for each team by row of the table
+- Construction of objects for each team (Team object)
+- Overwrite object variables using stored table data by searching corresponding row for each team and calling function to pull from specific cell from each row
--- dataPull.py
+++ dataPull.py
@@ -4,7 +4,9 @@ import re
 import selenium
 import pprint
 
-#Function to pull specified url. Takes a url as arg in string format, returns the html of the url.
+#--- Initial page and table function ---#
+
+### Function to pull specified url. Takes a url as arg in string format, returns the html of the url. ###
 def pagePull():
     url = "https://fbref.com/en/comps/9/Premier-League-Stats"
     response = requests.get(url)
@@ -12,19 +14,10 @@ def pagePull():
     return(soup)
 soup = pagePull()
 
+### Takes table id text and returns table element ###
 def pullTable(div):
     table = soup.find(id=div)
     return(table)
-
-regSeasonTableID = "switcher_results2023-202491"
-regSeasonTable = pullTable(regSeasonTableID)
-regSeasonTable_Rows = regSeasonTable.find_all("tr")
-#for row in regSeasonTable_Rows:
-#    print(row.text.strip())
-
-squadStandardStatsTableID = "switcher_stats_squads_standard"
-squadStandardStatsTable = pullTable(squadStandardStatsTableID)
-squadStandardStatsTable_Rows = squadStandardStatsTable.find_all("tr")
 
 ### Takes a single row from table and parses data out into seperate list items to be called ###
 def rowData(table, row):
@@ -32,70 +25,56 @@ def rowData(table, row):
     for data in table[row]:
         rows.append(data.text.strip())
     return(rows)
+
+
+#--- Individual Table Pulls ---#
+
+### Regular Season Stats Table ###
+regSeasonTableID = "switcher_results2023-202491"
+regSeasonTable = pullTable(regSeasonTableID)
+regSeasonTable_Rows = regSeasonTable.find_all("tr")
+
+### Squad Standard Stats Table ###
+squadStandardStatsTableID = "switcher_stats_squads_standard"
+squadStandardStatsTable = pullTable(squadStandardStatsTableID)
+squadStandardStatsTable_Rows = squadStandardStatsTable.find_all("tr")
+
 test = rowData(squadStandardStatsTable_Rows, 2)
 print(test)
 
+squadShootingTable = "switcher_stats_squads_shooting"
+squadPassingTable = "switcher_stats_squads_passing"
+squadPassingTypesTable = "switcher_stats_squads_passing_types"
+squadGCATable = "switcher_stats_squads_gca"
+squadDefensiveActionsTable = "switcher_stats_squads_defense"
+squadPosessionTable = "switcher_stats_squads_possession"
 
+#--- Data formatting and structuring ---#
 
-### Takes a team and data set to pass to the team object !!!NOT COMPATIBLE!!! ###
+### Takes a team and data set to pass to the team object !!!NOT COMPATIBLE WITH ALL TABLES!!! ###
 def squadSSTGen(team, data):
     team.players_used = data[1]
     team.avg_age = data[2]
     team.avg_possession = data[3]
 
+    team.goals = data[8]
+    team.assists = data[9]
+    team.goals_assists = int(team.goals) + int(team.assists)
+    team.pk_goals = data[12]
+    team.npk_goals = int(team.goals) - int(team.pk_goals)
+    team.pk_attempts = data[13]
+    team.yellows = data[14]
+    team.reds = data[15]
 
+    team.xG = data[16]
+    team.npxG = data[17]
+    team.xAG = data[18]
+    team.npxG_xAG = float(team.npxG) + float(team.xAG)
 
-def teamPull ():
-    url = "https://fbref.com/en/comps/9/Premier-League-Stats"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    team.prgC = data[20]
+    team.prgP = data[21]
 
-
-    #Basic stats for all PL Sides
-    """
-    Stats to Pull:
-        - Matches Played (MP)
-        - Wins (W)
-        - Draws (D)
-        - Losses (L)
-        - Goals For (GF)
-        - Goals Against (GA)
-        - Goal Difference (GD)
-        - Points (Pts)
-        - Points per Match (Pts/MP)
-        - Expected Goals For (xGF)
-        - Expected Goals Against (xGA)
-        - Expected Goal Difference (xGD)
-    """
-    squadStandardStatsTable = soup.find(id="switcher_stats_squads_standard")
-    """
-    Stats to Pull:
-        - Number of Players Used (#Pl)
-        - Average Age of Players Used (Age)
-        - Average Possession (Poss)
-        - Matches Played (MP)
-        - Assists (Ast)
-        - Goals + Assists (G+A)
-        - Penalty Kick Goals (PK)
-        - Goals - Penalty Goals (G-PK)
-        - Penalty Attempts (PKAtt)
-        - Yellow Cards (CrdY)
-        - Red Cards (CrdR)
-        - Non-Penalty Expected Goals (NPxG)
-        - Expected Assisted Goals (xAG)
-        - Non-Penalty xG + xAG (NPxG + xAG)
-        - Progressive Carries (PrgC)
-        - Progressive Passes (PrgP)
-    """
-    squadShootingTable = soup.find(id="switcher_stats_squads_shooting")
-    squadPassingTable = soup.find(id="switcher_stats_squads_passing")
-    squadPassingTypesTable = soup.find(id="switcher_stats_squads_passing_types")
-    squadGCATable = soup.find(id="switcher_stats_squads_gca")
-    squadDefensiveActionsTable = soup.find(id="switcher_stats_squads_defense")
-    squadPosessionTable = soup.find(id="switcher_stats_squads_possession")
-
-    resultsRows = squadStandardStatsTable.findAll("tr")
-    return(resultsRows)
+    team.xG_G_delta = round(float(team.goals) - float(team.xG), 2)
 
 
 class Team:
@@ -126,6 +105,10 @@ class Team:
         self.prgP = 0
 
 
+        self.xG_G_delta = 0
+
+#--- Team object generation and casting ---#
+
 ### Generate initial team objects ###
 Arsenal = Team("Arsenal", "ARS")
 AstonVilla = Team("Aston Villa", "AVL")
@@ -148,11 +131,49 @@ Tottenham = Team("Tottenham", "TOT")
 WestHam = Team("West Ham", "WHU")
 Wolves = Team("Wolves", "WOL")
 
-squadSSTGen(Arsenal, test)
+arsenalData = rowData(squadStandardStatsTable_Rows, 2)
+squadSSTGen(Arsenal, arsenalData)
 
+astonVillaData = rowData(squadStandardStatsTable_Rows, 3)
+squadSSTGen(AstonVilla, astonVillaData)
+
+bournemouthData = rowData(squadStandardStatsTable_Rows, 4)
+squadSSTGen(Bournemouth, bournemouthData)
+
+chelseaData = rowData(squadStandardStatsTable_Rows, 8)
+squadSSTGen(Chelsea, chelseaData)
+
+manCityData = rowData(squadStandardStatsTable_Rows, 14)
+squadSSTGen(ManchesterCity, manCityData)
+
+newcastleData = rowData(squadStandardStatsTable_Rows, 16)
+squadSSTGen(NewcastleUnited, newcastleData)
 
 
 ### Testing ###
 
-print(Arsenal.team_name, Arsenal.team_abv, Arsenal.avg_age, Arsenal.avg_possession)
-print(AstonVilla.team_name)
+def testPrint(team):
+    print(team.team_name, 
+          team.team_abv, 
+          team.players_used,
+          team.avg_age, 
+          team.avg_possession,
+          team.goals,
+          team.assists,
+          team.goals_assists,
+          team.pk_goals,
+          team.pk_attempts,
+          team.yellows,
+          team.reds
+          )
+
+testPrint(Arsenal)
+testPrint(AstonVilla)
+testPrint(Bournemouth)
+testPrint(Chelsea)
+testPrint(ManchesterCity)
+testPrint(NewcastleUnited)
+print(' ')
+list = [Arsenal, AstonVilla, Bournemouth, Chelsea, ManchesterCity, NewcastleUnited]
+for team in list:
+    print(str(team.team_name) + ": " + str(team.avg_possession) + "% " + str(team.goals) + ' goals ' + str(team.xG) + ' xG (' + str(team.xG_G_delta) + ') delta')
